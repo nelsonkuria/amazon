17ed1f34382120e22116b1b9a5168b66da5d4593
Nelson <nelson.kuria@smile.io>
2023-05-26 11:45:41 +0300

    added error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+  # rescue_from passes an optional parameter to the with: method
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  rescue_from ActiveRecord::RecordNotDestroyed, with: :not_destroyed
+
+  private
+
+  def not_found
+    render json: { error: 'Record not found!' }, status: :not_found
+  end
+
+  def not_destroyed(err)
+    render json: { errors: err.record.errors }, status: :unprocessable_entity
+  end
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,8 @@
 class BooksController < ApplicationController
+  # These will handle exceptions for all actions in this controller
+  # rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  # rescue_from ActiveRecord::RecordNotDestroyed, with: :not_destroyed
+
   def index
     render json: Book.all
   end
@@ -16,9 +20,19 @@ class BooksController < ApplicationController
   def destroy
     # The bang (!) after destroy returns true if the book is found and destroyed
     # Otherwise if it fails it will return an exception which we can handle
+    # If it fails to get the book, raises a RecordNotFound exception
+    # If it fails to destroy the book raises a RecordNotDestroyed exception
+    # -> Adding rescue blocks to controllers has some disadvantages e.g. can't resue them
+    # -> A better way to handle these is to use 'rescue_from' at the top of the controller
     Book.find(params[:id]).destroy!
 
     head :no_content # returns 204 response in the head of the request, no request body
+
+    # rescue ActiveRecord::RecordNotFound
+    #   render json: { error: "Book not found!"}, status: :not_found
+
+    # rescue ActiveRecord::RecordNotDestroyed
+    #   render json: {}, status: :unprocessable_entity
   end
 
   # we can use the default rails params (params[:something]) OR the gem 'rails-param'
@@ -28,4 +42,14 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :author)
   end
+
+  # rescue_from methods
+  #
+  # def not_found
+  #   render json: {}, status: :not_found
+  # end
+
+  # def not_destroyed
+  #   render json: {}, status: :unprocessable_entity
+  # end
 end
