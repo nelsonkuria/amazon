c46b250475f5e83dc0b8a0e1c3d44679c4d9ba66
Nelson <nelson.kuria@smile.io>
2023-05-26 11:19:03 +0300

    added book model validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+  # For more validations https://guides.rubyonrails.org/active_record_validations.html
+  validates :author, presence: true, length: { minimum: 3 }
+  validates :title, presence: true, length: { minimum: 3 }
 end
