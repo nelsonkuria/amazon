96e861060a6a1395361a06bf7fb83e98778b5122
Nelson <nelson.kuria@smile.io>
2023-08-05 10:42:23 +0300

    api response.body specs

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 
 describe 'Books API', type: :request do
+  let(:first_author) { FactoryBot.create(:author, first_name: 'George', last_name: 'Orwell', age: 60)}
+  let(:second_author) { FactoryBot.create(:author, first_name: 'H.G', last_name: 'Wells', age: 70)}
+
   describe 'GET /books' do
     before do
-      FactoryBot.create(:book, title: '1984', author: 'George Orwell')
-      FactoryBot.create(:book, title: 'The Time Machine', author: 'H.G Wells')
+      FactoryBot.create(:book, title: '1984', author: first_author)
+      FactoryBot.create(:book, title: 'The Time Machine', author: second_author)
     end
 
     it 'returns all books' do
@@ -16,6 +19,22 @@ describe 'Books API', type: :request do
       # response body, we need to actually create book records. rspec does't use
       # our default db, it creates a test db that needs to get seeded each time
       # therefore we install the gem `factory_bot_rails` to allow us to do this
+      expect(JSON.parse(response.body)).to eq(
+        [
+          {
+            'id' => 1,
+            'title' => '1984',
+            'author_name' => 'George Orwell',
+            'author_age' => 60
+          },
+          {
+            'id' => 2,
+            'title' => 'The Time Machine',
+            'author_name' => 'H.G Wells',
+            'author_age' => 70
+          }
+        ]
+      )
     end
   end
 
@@ -30,6 +49,14 @@ describe 'Books API', type: :request do
 
       expect(Author.count).to eq(1)
       expect(response).to have_http_status(:created)
+      expect(JSON.parse(response.body)).to eq(
+      {
+          'id' => 1,
+          'title' => 'The Martian',
+          'author_name' => 'Andy Weir',
+          'author_age' => 50
+        }
+      )
     end
   end
 
@@ -38,7 +65,7 @@ describe 'Books API', type: :request do
     # this makes our tests cleaner and more readable
     # note: let is lazily loaded i.e. won't run until we call `book.id` in the delete url
     # we add the bang (!) to make sure the factory gets created first when the test runs
-    let!(:book) { FactoryBot.create(:book, title: '1984', author: 'George Orwell') }
+    let!(:book) { FactoryBot.create(:book, title: '1984', author: first_author) }
 
     it 'deletes a book' do
       # book = FactoryBot.create(:book, title: '1984', author: 'George Orwell')
